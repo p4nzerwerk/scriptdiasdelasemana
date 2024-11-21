5269cccc9f59de07faf735beb0ebe9ad5bd5fac4
p4nzerwerk <pablo.comin1532005@gmail.com>
2024-11-21 15:05:27 +0100

    examen 21 de noviembre

---
+++ scriptpablocominmodificado.sh
@@ -0,0 +1,28 @@
+#!/usr/env/bin bash
+mkdir  2025
+cd ./2025
+mkdir enero
+cd ./enero
+mkdir semana1
+cd ./semana1
+mkdir miercoles jueves viernes sabado domingo
+cd ..
+cd ..
+mkdir semana2
+cd semana2
+mkdir lunes martes miercoles jueves viernes sabado domingo
+cd ..
+cd ..
+mkdir semana3
+cd semana3
+mkdir lunes martes miercoles jueves viernes sabado domingo
+cd ..
+cd ..
+mkdir semana4
+cd semana4
+mkdir lunes martes miercoles jueves viernes sabado domingo
+cd ..
+cd ..
+mkdir semana5
+cd semana5
+mkdir lunes martes miercoles jueves viernes
